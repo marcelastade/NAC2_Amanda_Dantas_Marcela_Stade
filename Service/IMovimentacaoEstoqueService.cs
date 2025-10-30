fd11fa17359f78b63a786268688a11fab030942b
Marcela Stade <78057087+marcelastade@users.noreply.github.com>
2025-10-30 14:41:24 -0300

    Delete Service directory

--- Service/IMovimentacaoEstoqueService.cs
+++
@@ -1,9 +0,0 @@
-﻿using NAC2.Domain;
-
-namespace NAC2.Service
-{
-    public interface IMovimentacaoEstoqueService { 
-        
-        Task<MovimentacaoEstoque> AddAllMovimentacoesAsync(MovimentacaoEstoque movimentacao);
-    }
-}
